e364dfb697d73a1247b4143680b8e99a30d48741
hoitran6868 <abc@example.com~clear>
2024-06-22 10:30:06 +0900

    Created Featured Products - HTML

--- src/utils.js
+++ src/utils.js
@@ -7,7 +7,7 @@ const allProductsUrl = 'https://www.course-api.com/javascript-store-products';
 // temporary single product
 // 'https://www.course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog'
 const singleProductUrl =
-  'https://www.course-api.com/javascript-store-single-product';
+  'https://www.course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog';
 
 const getElement = (selection) => {
   const element = document.querySelector(selection);
