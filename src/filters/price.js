e27db039b8ecc5caf14d7cdf582b195cc08e04ba
hoitran6868 <abc@example.com~clear>
2024-06-23 14:42:48 +0900

    Price Filter

--- src/filters/price.js
+++ src/filters/price.js
@@ -1,6 +1,30 @@
 import { getElement } from '../utils.js';
 import display from '../displayProducts.js';
 
-const setupPrice = () => {};
+const setupPrice = (store) => {
+  const priceInput = getElement('.price-filter');
+  const priceValue = getElement('.price-value');
+
+  // setup filter
+  let maxPrice = store.map((product) => product.price);
+  maxPrice = Math.max(...maxPrice); // to spread out array values
+  maxPrice = Math.ceil(maxPrice / 100);
+  priceInput.value = maxPrice;
+  priceInput.max = maxPrice;
+  priceInput.min = 0;
+  priceValue.textContent = `Value : $${maxPrice}`;
+
+  priceInput.addEventListener('input', function () {
+    // converts value of priceInput from string to number
+    const value = parseInt(priceInput.value);
+    priceValue.textContent = `Value : $${value}`;
+    let newStore = store.filter((product) => product.price / 100 <= value);
+    display(newStore, getElement('.products-container'));
+    if (newStore.length < 1) {
+      const products = getElement('.products-container');
+      products.innerHTML = `<h3 class="filter-error">Sorry, No Products Matched Your Search</h3>`;
+    }
+  });
+};
 
 export default setupPrice;
